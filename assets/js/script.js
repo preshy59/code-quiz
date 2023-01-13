fb07a1ad6fa34efb66fa558e4421eed01e9e125d
Ebele Okemba <okemba50@gmail.com>
2023-01-13 20:05:48 +0000

    create the script.js and highscore.js, also, create so variable to store the varaible in html as an element in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//creating and storing variables
+ let questionEl = document.querySelector("#questions");
+ let startBtn = document.querySelector("#start");
+ let choiceEl = document.querySelector("#choices");
+ let submitBtn = document.querySelector("#submit");
+ let questionTitleEl = document.querySelector("#question-title");
+ let timeEl = document.querySelector("#time");
+ let startScreenEL = document.querySelector("#start-screen");
+ let endScreenEl = document.querySelector("#end-screen");
+ let finalScoreEl = document.querySelector("#final-score");
+ let initalsEL = document.querySelector("#initials");
+ let feedbackEl = document.querySelector("#feedback");
+
+ 
