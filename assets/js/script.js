626235a7e17f5a8c1228a94e1e9cdabce01cdb8b
Ebele Okemba <okemba50@gmail.com>
2023-01-14 13:36:24 +0000

    added arrays of question

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,39 @@
  let initalsEL = document.querySelector("#initials");
  let feedbackEl = document.querySelector("#feedback");
 
+ //creating arrays of questions as an object
+ let questions =[
+    {
+        title: "What is programming language?", 
+        options:["stragies that are used in solving problem", "written language that tells computers what to do", "language understood by aliens", "method of communication using by Americans"],
+        answer: "written language that tells computers what to do"
+    },
+
+    {
+        title:"Example of programming language?",
+        options:["JavaScript","Spanish", "English", "Germany"],
+        answer: "JavaScript"
+    },
+
+    {
+        title:"Type of programming language?",
+        options:["High Level", "Body language", "Speech language", "Love language"],
+        answer: "High Level"
+    },
+
+    {
+        title:"Which of the following is not a JavaScript function?",
+        option:["alert()", "eval()", "log()", "none"],
+        answer: "none"
+    },
+
+    {
+        title:"what function can be used to translate a string to an int?",
+        option: ["Stringify","toFloat", "valueOf","toString"],
+        answer: "toString"
+    }
+
+
+
+]
  
